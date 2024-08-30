8c1a008b3817daeb5b2b9b87c73ada2db5a20d71
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2024-08-30 00:50:25 -0400

    still to implement it

--- source/project.js
+++ source/project.js
@@ -2,3 +2,11 @@
 
 
 
+
+
+function ProjectPage(Parent){
+    if (localStorage.length === 0){
+        let maindiv = document.querySelector(".main .maindiv");
+        document.querySelector(".addtaskbtn").innerHTML = "Add Project";
+    }
+}
